b7ba3e849fabddf0597489022279fa2773416cd1
Rick <richims026@gmail.com>
2025-03-24 08:06:06 -0500

    Init

---
+++ main.R
@@ -0,0 +1,45 @@
+# LoadLibrary
+library(tidyverse)
+library(magrittr)
+library(ggdark)
+library(expss)
+library(lubridate)
+library(pacman)
+
+# .lintr
+text <- c(
+  "linters: linters_with_defaults(",
+  "  assignment_linter(allow_pipe_assign = TRUE)",
+  "  )"
+)
+
+pathfile <- file.path(getwd(), ".lintr")
+
+writeLines(text, pathfile)
+
+options("width" = 10000)
+getOption("width")
+
+# Directorios
+dir.create("Rdownloads")
+file.create("test.R")
+file.create("rem.md")
+file.create(".gitignore")
+
+# Procesamiento
+
+p_load(palmerpenguins)
+
+# Modelamiento
+
+mpg %>% str()
+
+mpg %>%
+  ggplot(aes(displ, cty)) +
+  geom_jitter(aes(col = factor(year))) +
+  geom_smooth(method = "lm") +
+  dark_theme_grey()
+
+penguins %>% head()
+
+#
---
+++ rem.md
@@ -0,0 +1,12 @@
+# Apuntes
+
+**Hoy empieza esta aventura una vez más, pero esta vez voy a ganar de una forma abrumadora**
+
+- [--] R gráficos
+- [--] R package
+- [--] Funciones Prueba de Hipostesis
+- [--] Debugear En Vscode
+- [--] Book R
+- [--] R + C++
+- [--] 
+- [--] 
